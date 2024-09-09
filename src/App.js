4ab30d21b4db81e790e536c05ce9373c655441c9
Patrik Cmil <patrik.cmil@student.tuke.sk>
2024-09-09 13:10:28 +0200

    dalsia mensia uprava

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import axios from 'axios';
 
 function App() {
   const [text, setText] = useState('');
-  const [message, setMessage] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.post('http://hockey-app-backend.up.railway.app/api/text', { text });
+      console.log('Text added:', response.data);
       setText('');
-      setMessage(`Úspešne uložené: ${response.data.content}`);
     } catch (error) {
       console.error(error);
-      setMessage('Nastala chyba pri ukladaní.');
     }
   };
 
@@ -24,13 +22,12 @@ function App() {
       <form onSubmit={handleSubmit}>
         <input
           type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
           placeholder="Zadajte text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}   
         />
         <button type="submit">Odoslať</button>
       </form>
-      {message && <p>{message}</p>}
     </div>
   );
 }
