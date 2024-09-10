4b8eba6263f7cd9e39205bacf5908d41f954713b
Patrik Cmil <patrik.cmil@student.tuke.sk>
2024-09-10 20:32:56 +0200

    zmena source loga

--- src/Logos.js
+++ src/Logos.js
@@ -1,7 +1,6 @@
 const logos = {
-    "Kosice": require("./logos/Kosice.png"),
-    "Trencin": require("./logos/Trencin.png",),
-    "Slovan Bratislava": require("./logos/Bratislava.png"),
+    "Kosice": "https://upload.wikimedia.org/wikipedia/en/7/75/HC_Ko%C5%A1ice_logo.svg",
+    "Slovan Bratislava": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJP6AynDyH1b-lTJLl4baqMdU-YS4ejVr9fw&s",
 }
 
 export default logos;
